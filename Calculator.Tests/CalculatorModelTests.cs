cf11b0bad422c1bb0d08caca8a476ac72d32631f
Jimi Takamäki <jimi.takamaki@gmail.com>
2022-06-20 11:12:16 +0300

    Add more simple tests

--- Calculator.Tests/CalculatorModelTests.cs
+++ Calculator.Tests/CalculatorModelTests.cs
@@ -14,5 +14,35 @@ namespace Calculator.Tests
             double result = calculator.Calculate(calculation);
             Assert.AreEqual(expectedResult, result);
         }
+
+        [DataTestMethod]
+        [DataRow("5-5", 0)]
+        [DataRow("1-900-2", -901)]
+        public void Calculate_subtraction_differentValues(string calculation, double expectedResult)
+        {
+            CalculatorModel calculator = new CalculatorModel();
+            double result = calculator.Calculate(calculation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("5*5", 25)]
+        [DataRow("1*900*2", 1800)]
+        public void Calculate_multiplication_differentValues(string calculation, double expectedResult)
+        {
+            CalculatorModel calculator = new CalculatorModel();
+            double result = calculator.Calculate(calculation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("5/5", 1)]
+        [DataRow("12/2/3", 2)]
+        public void Calculate_division_differentValues(string calculation, double expectedResult)
+        {
+            CalculatorModel calculator = new CalculatorModel();
+            double result = calculator.Calculate(calculation);
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
